00a8e2bed450e81b7c024fa07c21000a32ecda9f
lh <1072344372@qq.com>
2023-11-12 23:56:53 +0800

    Initial commit

---
+++ src/main/java/asia/lhweb/usercenter/model/User.java
@@ -0,0 +1,6 @@
+package asia.lhweb.usercenter.model;/** 
+ * @author :罗汉
+ * @date : 2023/11/12 
+ */
+public class User {
+}
---
+++ src/test/java/asia/lhweb/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package asia.lhweb.usercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
